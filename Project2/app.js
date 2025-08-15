4eea39a77c49b7cbc2273ad11662c5c9e87aad57
george <malalelufu26@gmail.com>
2025-08-14 22:08:03 -0400

    weather project progress

--- Project2/app.js
+++ Project2/app.js
@@ -1,14 +1,39 @@
+// app.js
+
 const apiKey = "8bfb4cc29e3c8ea2cab18f5e2cbe0ffb";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?qunits=metric&q=nairobi";
-async function checkWeather() {
-       const response = await fetch(apiUrl + `&appid = ${apiKey}`);
-       var data = await response.json();
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-        console.log(data);
+async function weatherCheck (city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
 
-        document.querySelector('.city').innerHTML = data.name;
-        document.querySelector('.temp').innerHTML = data.main.temp + "°C";
-        document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
-        document.querySelector('.temp').innerHTML = data.wind.speed + "km/h";
-                }
-checkWeather();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+    if(data.weather[0].main == "clouds") {
+        weatherIcon.src = "rainy-day.png";
+    }
+    else if(data.weather[0].main == "clear") {
+        weatherIcon.src = "sun.png";
+    }
+    else if (data.weather[0].main == "Rain"){
+        weatherIcon.src = "rainy-day.png";
+    }
+
+    else if (data.weather[0] == "Drizzle") {
+        weatherIcon.src = "cloudy.png";
+    }
+
+    else if (data.weather[0] == "Mist") {
+        weatherIcon.src = "cloud(1).png"
+    }
+}
+searchBtn.addEventListener("click", () =>{
+weatherCheck(searchBox.value);
+})
